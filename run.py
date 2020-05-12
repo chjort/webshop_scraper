ab787cb6e500e0e5510d61dbd03b3a6bb5a588b1
chjort <christoffer.hjort1995@gmail.com>
2020-05-12 12:34:45 +0200

    Remove data from git repo

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
+import os
+import datetime
+
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 from webshop_scraper.spiders.amazon_scraper import AmazonScraper
 
+crawl_time = int(datetime.datetime.now().timestamp())
+save_dir = os.path.join("data", str(crawl_time))
+
 settings = get_project_settings()
 process = CrawlerProcess(settings=settings)
-process.crawl(AmazonScraper)
+process.crawl(AmazonScraper, n_pages=2, product_save_dir=save_dir)
 process.start()
